37b0fc4d484998f87fc7e617c4ed363367286731
Gayaththri <sriigaya3@gmail.com>
2024-06-01 11:52:51 +0530

    Integrated Todo component with TodoWrapper, displaying tasks dynamically

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -17,7 +17,9 @@ const TodoWrapper = () => {
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
-      <Todo />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   );
 };
